740f933810fa03e0de2b82d4c18b8be2dd4629fe
j41b4l4j1 <j41b4l4j1@gmail.com>
2019-06-01 03:15:38 -0700

    first commit

---
+++ src/app/welcome-component/welcome-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-welcome-component",
+  templateUrl: "./welcome-component.component.html",
+  styleUrls: ["./welcome-component.component.css"]
+})
+export class WelcomeComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  loadResult(event) {
+    alert('button clicked');
+  }
+}
